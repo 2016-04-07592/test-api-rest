7013a7d6957257c6a405560a4f54366d254c4fa9
2016-04-07592 <josephkwagilwa@gmail.com>
2018-11-27 11:26:35 +0300

    After Validators

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true
     },
     completed: {
         type: Boolean
@@ -16,13 +17,26 @@ var Todo = mongoose.model('Todo', {
     }
 });
 
+// var newTodo = new Todo({
+//     text: 'Cook Dinner'
+// });
+// newTodo.save().then((doc) => {
+//         console.log('Saved todo', doc);
+//     },
+//     (err) => {
+//         console.log('Unable to save todo', err); 
+//     }
+// );  
+
 var newTodo = new Todo({
-    text: 'Cook Dinner'
+
 });
+
 newTodo.save().then((doc) => {
-        console.log('Saved todo', doc);
+        console.log('saved todo succesfully ', JSON.stringify(doc, undefined, 3));
+
     },
     (err) => {
-        console.log('Unable to save todo', err);
+        console.log('Something went wrong', err);
     }
 );
